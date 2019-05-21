4cc63b7aa06d3ba8cb634b3e0f7e6a5865755955
Your Name <Jarinfarhad58>
2019-05-21 10:15:17 +0600

    Welcome form

--- MyWinApp/MyWinApp/HomeForm.Designer.cs
+++ MyWinApp/MyWinApp/HomeForm.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ClickButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // ClickButton
+            // 
+            this.ClickButton.Location = new System.Drawing.Point(234, 98);
+            this.ClickButton.Name = "ClickButton";
+            this.ClickButton.Size = new System.Drawing.Size(261, 99);
+            this.ClickButton.TabIndex = 0;
+            this.ClickButton.Text = "Click Me";
+            this.ClickButton.UseVisualStyleBackColor = true;
+            this.ClickButton.Click += new System.EventHandler(this.ClickButton_Click);
             // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClickButton);
             this.Name = "HomeForm";
             this.Text = "Home";
             this.ResumeLayout(false);
@@ -42,6 +54,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ClickButton;
     }
 }
 
--- MyWinApp/MyWinApp/HomeForm.cs
+++ MyWinApp/MyWinApp/HomeForm.cs
@@ -16,5 +16,10 @@ namespace MyWinApp
         {
             InitializeComponent();
         }
+
+        private void ClickButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome ");
+        }
     }
 }
